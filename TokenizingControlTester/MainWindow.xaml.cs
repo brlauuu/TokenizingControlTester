a61c6acab731be6017318d63ecb6fca84e02edfa
Đorđe Relić <djordje.relic@outlook.com>
2017-03-21 22:24:20 +0100

    Added extracting of tokens.

--- TokenizingControlTester/MainWindow.xaml.cs
+++ TokenizingControlTester/MainWindow.xaml.cs
@@ -39,5 +39,41 @@ namespace TokenizingControlTester
                                              return null;
                                          };
         }
+
+        private void extractTokens_Click(object sender, RoutedEventArgs e)
+        {
+            extractedTokens.Text = "";
+            foreach (TextBlock tb in FindVisualChildren<TextBlock>(Tokenizer))
+            {
+                extractedTokens.Text += tb.Text + ";" ;
+            }
+
+        }
+        public IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+
+        }
+
+
+        private void addSpecialToken_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
